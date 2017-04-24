18bcb2c33a8b18725abd7f33d4ee1f01cfebaad5
Bailey Kursar <bkursar@gmail.com>
2017-04-24 20:37:48 +0100

    Added a cat route and views folder, with html

--- app.rb
+++ app.rb
@@ -1,1 +1,28 @@
 require 'sinatra'
+
+get '/' do
+  'Hello!'
+end
+
+get '/secret' do
+  'Secret handshake'
+end
+
+get '/another-route' do
+  'Another route...'
+end
+
+get '/funtimes' do
+  'FUNTIMES!'
+end
+
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
